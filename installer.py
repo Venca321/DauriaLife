0f933b429e5ab50f8d0e3e15dcbdba0c05766a1d
Venca321 <login@vaclavparma.cz>
2023-05-28 11:21:40 +0200

    Fix github action

--- installer.py
+++ installer.py
@@ -12,11 +12,13 @@ os.system('pip install -r requirements.txt')
 #os.chdir("../")
 
 #Make folders
+LOCATION = os.getcwd()
+print(LOCATION)
 FOLDERS = ["Data/Settings"]
 for folder in FOLDERS:
-    try: os.mkdir(folder)
+    try: os.mkdir(f"{LOCATION}/{folder}")
     except: None
 
 #Copy files
-shutil.copy("Data/Defaults/.env.example", "Data/Settings/.env")
-shutil.copy("Data/Defaults/default_settings.json", "Data/Settings/settings.json")
+shutil.copy(f"{LOCATION}/Data/Defaults/.env.example", f"{LOCATION}/Data/Settings/.env")
+shutil.copy(f"{LOCATION}/Data/Defaults/default_settings.json", f"{LOCATION}/Data/Settings/settings.json")
