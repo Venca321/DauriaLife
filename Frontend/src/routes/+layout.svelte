56b7171c94cfda293d60604ee2d8ba2d0e753021
Venca321 <login@vaclavparma.cz>
2023-05-30 20:30:51 +0200

    Add frontend

---
+++ Frontend/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<slot />
+
+<style>
+	:global(:root) {
+		--color-background: #24242f;
+		--color-text: #ffffff;
+		--color-error: #ff370068;
+
+        --color-gray: #626280;
+        --color-primary: #42b17d;
+        --color-secondary: #d3e71d;
+	}
+</style>
---
+++ Frontend/src/routes/+page.svelte
@@ -0,0 +1,52 @@
+
+<body>
+    <div id="container">
+        <section id="dashboard">
+            <p>Dashboard</p>
+        </section>
+
+        <section id="automation">
+            <p>Automatizace</p>
+        </section>
+
+        <section id="control">
+            <p>Test</p>
+        </section>
+
+        <style>
+            body, html {
+                height: 100%;
+                margin: 0;
+                overflow: hidden;
+            }
+        </style>
+    </div>
+</body>
+
+<style>
+    #container {
+        height: 100%;
+        width: 100%;
+        overflow-y: scroll;
+        scroll-snap-type: y mandatory;
+    }
+
+    section {
+        height: 100%;
+        width: 100%;
+        scroll-snap-align: start;
+        display: flex;
+    }
+    
+    #dashboard {
+        background-color: #ffcc00;
+    }
+
+    #automation {
+        background-color: #00ccff;
+    }
+
+    #control {
+        background-color: #cc00cc;
+    }
+</style>
