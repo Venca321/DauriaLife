f414d60228c833a5b352c6128eae5ff3df216500
Venca321 <login@vaclavparma.cz>
2023-06-11 09:58:38 +0200

    Add multiprocessing

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 
 from Backend.core import *
-import os, time
+import os, time, multiprocessing
+
+def tester_task():
+    while True:
+        time.sleep(5*60)
+        if not Tests.run(): raise
+
+def weather_updater_task():
+    WEATHER_DELAY = Data.weather.update_time_minutes*60
+    time.sleep(10)
+    while True:
+        start = time.time()
+        weather.update()
+        time.sleep(WEATHER_DELAY-(time.time()-start))
 
 if __name__ == "__main__":
     os.system("clear")
@@ -14,3 +27,5 @@ if __name__ == "__main__":
     time.sleep(0.5)
 
     #multiprocessing
+    multiprocessing.Process(target=weather_updater_task, daemon=True).start()
+    tester_task()
